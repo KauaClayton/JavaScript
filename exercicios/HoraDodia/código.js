04dd7cc606862efa4d62fee5826ca1847f0cd677
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-03-19 12:50:30 -0300

    Terminei o exercício dia e noite

--- exercicios/HoraDodia/código.js
+++ exercicios/HoraDodia/código.js
@@ -2,9 +2,22 @@ var agora = new Date()
 var hora = agora.getHours()
 var texto = window.document.querySelector('p#te')
 
-if(6 <= hora &&  hora < 13){
-    document.body.style.background = "#E6CB95"
-    texto.innerHTML = `Agora são ${hora} horas.`
-    document.querySelector('div#img').backgroung = ''
+texto.innerHTML = `Agora são ${hora} horas.`
+if(6 <= hora &&  hora < 12){
+    document.body.style.background = "#E6CB95"   
+    document.body.querySelector('div#caixa').querySelector('div#img').style = 'background-image:url("http://apfel.com.br/wp-content/uploads/2021/01/flor-e-sol.jpg")'
+}
+else if(12 <= hora && hora < 18){
+    document.body.style.background = '#9F6C49'
+    document.body.querySelector('div#caixa').querySelector('div#img').style = 'background-image: url("https://static.vecteezy.com/ti/fotos-gratis/p1/1752040-navio-no-mar-durante-o-por-do-sol-gratis-foto.jpeg")'
     
+}
+else if (18 <= hora && hora < 24){
+    document.body.style.background = '#5B5D63'
+    document.body.querySelector('div#caixa').querySelector('div#img').style = 'background-image: url("http://2.bp.blogspot.com/-3hOI92nt1Xc/UK0wuND78ZI/AAAAAAAAQXA/qJQwhyiy35w/s1600/163393_279721975458093_1416357960_n.jpg")'
+}
+
+else if (0 <= hora && hora < 6){
+    document.body.style.background = '#2F302C'
+    document.body.querySelector('div#caixa').querySelector('div#img').style = 'background-image: url("https://www.jornaldocomercio.com/_midias/jpg/2021/04/27/img_7666-9303421.jpg")'
 }
