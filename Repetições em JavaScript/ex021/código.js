a2edec296dadd403462b628238eb892fd3cba79b
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-04-11 17:08:02 -0300

    javascript

---
+++ Repetições em JavaScript/ex021/código.js
@@ -0,0 +1,10 @@
+function clicou(){
+    var resp = document.querySelector('p#res')
+    resp.innerHTML += '<h2>Contando de 1 até 10</h2>'
+    s = 0
+    while(s <= 9){
+        s ++
+        resp.innerHTML += s + '\uD83D\uDC49'
+    }                                                                                                                                                                                      
+    resp.innerHTML + = '10'
+}
