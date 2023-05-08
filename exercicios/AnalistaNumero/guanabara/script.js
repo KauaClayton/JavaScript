2a7d7a69f35b8252a1a6371cea64d48794f1899e
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-05-08 15:57:46 -0300

    javascript

--- exercicios/AnalistaNumero/guanabara/script.js
+++ exercicios/AnalistaNumero/guanabara/script.js
@@ -24,10 +24,45 @@ function inLista(n, l){
 
 function adicionar(){
     if(isnumero(num.value) && !inLista(num.value, valores)){
-        
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }
     else{
         window.alert('Valor invalido ou ja encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('Coloque valores antes de finalizar!!!')
+    }
+    else{
+        var total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let media = 0
+        let soma = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                 maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>O total de números cadastrados é ${total}</p>`
+        res.innerHTML += `<p>O menor valor registrado foi ${menor}</p>`
+        res.innerHTML += `<p>O maior valor registrado foi ${maior}</P>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+        res.innerHTML += `<p>A media tirada da lista foi ${soma/valores.length}`
+
     }
 }
 
