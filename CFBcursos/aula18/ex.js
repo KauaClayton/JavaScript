92fecd724bd633e439b06058e612ab378f56fae4
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-05-18 16:09:58 -0300

    javascript

---
+++ CFBcursos/aula18/ex.js
@@ -0,0 +1,12 @@
+
+
+
+let n = 10
+let p = n
+let tot = 1
+while(n > 0){
+    tot *= n
+    n--
+}
+
+console.log(`Fatorial de ${p} é = ${tot}`)
---
+++ CFBcursos/aula20/ex.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let n = 0
+let max = 100
+
+while(n < max){
+    console.log('CFB CURSOS' + n)
+    if(n > 10){
+        break
+    }
+    n++}
+    
+
+console.log('fim do programa')
