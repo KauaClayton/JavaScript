e6c3eafec5a9a58d58c9410b50063d38dbfe578e
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-06-08 13:32:09 -0300

    att

---
+++ CFBcursos/aula36/ex.js
@@ -0,0 +1,24 @@
+"use strict"
+
+
+const caixa1 = document.querySelector('#caixa01')
+const btn = document.querySelector('button#button')
+const caixa2 = document.querySelector('#caixa02')
+const cursos = [...document.querySelectorAll('.curso')]
+
+cursos.map((el)=>{el.addEventListener('click', (evt)=>{
+    const curso = evt.target
+    curso.classList.toggle('selecionado')
+    
+})})
+
+btn.addEventListener('click', ()=>{
+    const selecionados = [...document.querySelectorAll('.selecionado')]
+    selecionados.map((el)=>{
+        caixa2.appendChild(el)
+        if(selecionados.includes(el) == false){
+            el.parentElement = caixa1
+        }       
+    })
+})
+
