8c1e753cfbf53aef5c73a2c14bbea0cd342b1512
MefistoCly <121896566+MefistoCly@users.noreply.github.com>
2023-06-20 16:44:18 -0300

    aaaaaaaaaaaaaaa

---
+++ CFBcursos/aula51/ex.js
@@ -0,0 +1,13 @@
+const verde = document.querySelector('div.verde')
+const button = document.querySelector('button#button')
+const resul = document.querySelector('p#result')
+const array = [1, 2, 3, 4, 5]
+let aux = []
+
+verde.innerHTML +=  `<br>[${array}]`
+
+button.addEventListener('click', (evt)=>{
+
+})
+
+
