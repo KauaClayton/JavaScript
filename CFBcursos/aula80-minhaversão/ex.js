29bbfe7d9bf0e5024854ec3dfffc9bf0e9c9d6f9
Kauaclaton <121896566+KauaClayton@users.noreply.github.com>
2023-07-10 16:26:09 -0300

    Tentando fazer o exxx

--- CFBcursos/aula80-minhaversão/ex.js
+++ CFBcursos/aula80-minhaversão/ex.js
@@ -1,9 +1,26 @@
 const dataano = document.querySelector('#data')
-const dados=()=>{
+const relogio = document.querySelector('#relogio')
+const botao_setar_alarme = document.querySelector('#btn_ativar')
+const botao_parar_alarme = document.querySelector('#btn_parar')
+const horalarme = document.querySelector('#horaalarme')
+const dados=()=>{  
     const data = new Date()
     dataano.innerHTML = data.toLocaleDateString()
+    relogio.innerHTML = data.toLocaleTimeString() 
 }
 
 setInterval(() => {
     dados()
+
 }, 1000);
+
+botao_setar_alarme.addEventListener('click', ()=>{
+    const tempoalarme = document.querySelector('#inum').value
+    const data = new Date()  
+    const dataalterada = new Date()
+    dataalterada.setSeconds(dataalterada.getSeconds() + tempoalarme/0001)
+    horalarme.innerHTML =  dataalterada.toLocaleTimeString(); 
+})
+botao_parar_alarme.addEventListener('click', ()=>{
+    horalarme.innerHTML = '00:00:00'
+})
