fce4d0309713a5287e1b53e591465205bdb32b19
Kauaclaton <121896566+KauaClayton@users.noreply.github.com>
2023-09-18 14:52:53 -0300

    ai

---
+++ CFBcursos/aula115/ex.js
@@ -0,0 +1,6 @@
+let numeros = [10,20,30,40,50,60,70,80,90]
+let a,b,c,d 
+[a,b,c,...d]= numeros
+let obj={nome:'Bruno',canal:'CFBCursos' }
+let{nome,canal}= obj
+console.log(nome, canal)
---
+++ CFBcursos/aula116/ex.js
@@ -0,0 +1,7 @@
+let numeros = ()=>{
+        return[10,20,30,40]
+}
+
+let a=10,b=5,c,d
+[a,b,c,d]=numeros()
+console.log(a,b,c,d)
