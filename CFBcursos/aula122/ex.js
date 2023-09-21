84fe03602594ddf5891b72ab468a659107b1bd65
Kauaclaton <121896566+KauaClayton@users.noreply.github.com>
2023-09-21 15:20:00 -0300

    aaaaaaaaaaaa

--- CFBcursos/aula122/ex.js
+++ CFBcursos/aula122/ex.js
@@ -7,8 +7,13 @@ btn_texto.addEventListener('click',(evt)=>{
 })
 
 let num = 10
-window.localStorage.setItem('numero', num)
-alert(localStorage.getItem('numero'))
-localStorage.setItem('nome','bruno')
-localStorage.setItem('canal','bruno')
-localStorage.clear()
+// window.localStorage.setItem('numero', num)
+
+// localStorage.setItem('nome','bruno')
+// localStorage.setItem('canal','bruno')
+// alert(localStorage.length)
+
+sessionStorage.setItem('nome','bruno')
+sessionStorage.setItem('canal','bruno')
+alert(sessionStorage.length)
+
---
+++ CFBcursos/aula123/ex.js
@@ -0,0 +1,3 @@
+const endpoint = 'https://cfbcursos--kauaclaytonng.repl.co'
+let res = fetch(endpoint)
+console.log(res)
