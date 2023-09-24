772f1a2c21241970478e904db076f3dec59c6733
Kauaclaton <121896566+KauaClayton@users.noreply.github.com>
2023-09-24 14:04:22 -0300

    hijos de pueblo

---
+++ CFBcursos/aula126/ex.js
@@ -0,0 +1,41 @@
+const p_temperatura = document.querySelector('#p_temperatura')
+const p_nivel = document.querySelector('#p_nivel')
+const p_pressao = document.querySelector('#p_pressao')
+const texto = document.querySelector('#btn_texto')
+const obter_dados = ()=>{
+    const endpoint = 'https://cfbcursos.kauaclaytonng.repl.co/'
+    let res = fetch(endpoint)
+    .then(res=>res.json())
+    .then(dados=>{
+        console.log(dados)
+        p_temperatura.innerHTML = 'temperatura:' + dados.temp
+        p_nivel.innerHTML = 'nível:' + dados.nivel
+        p_pressao.innerHTML = 'pressão' + dados.pressão 
+    })
+}
+
+// let intervalo = setInterval(obter_dados, 3000)
+
+let dados={
+    nome:'Bruno',
+    canal:'CFBcursos',
+    curso:'JavaScript'
+}
+
+let cabecalho={
+    method:'POST',
+    body:JSON.stringify(dados)
+}
+
+const gravardados=()=>{
+    const endpoint = 'https://cfbcursos.kauaclaytonng.repl.co/'
+    fetch(endpoint, cabecalho)
+    .then(res=>res.json())
+    .then(res=>{
+        console.log(res)
+    })
+}
+
+texto.addEventListener('click', ()=>{
+    gravardados()
+})
