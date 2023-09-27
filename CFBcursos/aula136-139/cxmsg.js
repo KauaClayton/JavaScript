3fec47ade6a330de65cd95c09f67849d17628d45
Kauaclaton <121896566+KauaClayton@users.noreply.github.com>
2023-09-27 16:13:04 -0300

    eu sou exagerado

--- CFBcursos/aula136-139/cxmsg.js
+++ CFBcursos/aula136-139/cxmsg.js
@@ -3,25 +3,55 @@ class Cxmsg{
     texto=null
     cor=null
     destino=null
-    div_msg = null
+    divmsg = null
     constructor(config){
         this.titulo=config.titulo
         this.texto=config.texto
         this.cor=config.cor
-        this.destino=document.body
+        this.destino = document.body
     }
     mostrar=()=>{ 
-        this.div_msg=document.createElement('div')
-        const estilo_div_msg = 
-        "display:flex;"+
-        "justify-content:center;"+
-        "align-items:center"+
-        "position:absolute;"+
-        "top:0px;"+
-        "left:0px;"
-        "width:100vw;"+
-        "height:100vh;"+
-        "background-color:rgba(0,0,0,0.7)"
+        this.divmsg=document.createElement('div')
+        const estilo_divmsg = 
+            "display: flex;"+
+            "justify-content: center;"+
+            "align-items: center;"+
+            "position: absolute;"+
+            "top: 0px;"+
+            "left: 0px;" +
+            "width: 100%;"+
+            "height: 100vh;"+
+            "background-color: rgba(0,0,0,0.7);"
+         this.divmsg.setAttribute('id', 'divmsg')
+         this.divmsg.setAttribute('style',estilo_divmsg)
+         this.destino.prepend(this.divmsg)
+         
+         const areacxmsg = document.createElement('div')
+         const estilo_areaCxmsg=
+            'display: flex;'+
+            'justify-content: flex-start;'+
+            'align-items: flex-start;'+
+            'flex-direction: column;'+
+            'width:300px;'
+            areacxmsg.setAttribute('style', estilo_areaCxmsg)
+            this.divmsg.appendChild(areacxmsg)
+
+       const titulo = document.createElement('div')
+       const estilotitulo=
+            'display: flex;'+
+            'justify-content: flex-start;'+
+            'align-items:center;'+
+            'width:100%;'+
+            'background-color:'+this.cor+';'+
+            'color:#fff;'+
+            'padding:5px;'+
+            'border-radius:5px 5px 0px 0px'
+       titulo.setAttribute('style', estilotitulo)
+       titulo.innerHTML = this.titulo
+       areacxmsg.appendChild(titulo)
+
+
+        
     }
     ocultar=()=>{}
 }
